0fd56abfba81aecd7b626b93188c6c2599417eed
dianabanguera <dianabangueravillalobo@gmail.com>
2022-02-10 11:51:40 -0800

    puts imprime

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
  * main - print "Programming is like building a multilingual puzzle"
- * void - no tiene valor de retorno
- * Return = 0
+ * Return : 0
  */
 int main(void)
 {
