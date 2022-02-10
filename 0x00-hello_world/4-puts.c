db897171039b238be4d68052caf16e3a6150fef6
dianabanguera <dianabangueravillalobo@gmail.com>
2022-02-10 10:41:42 -0800

    imprime

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,5 @@
-gcc -c puts = ("Programming is like building a multilingual puzzle");
-return 0;
+#include <stdio.h>
+{
+puts("\ "Programming is like building a multilingual puzzle");
+return (0);
+}
