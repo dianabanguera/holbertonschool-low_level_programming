6c4253226059c342694848a530e753bb813d8661
dianabanguera <dianabangueravillalobo@gmail.com>
2022-02-10 11:40:10 -0800

    puts imprime

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - print word "Programming is like building a multilingual puzzle"
  * Return = 0
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
