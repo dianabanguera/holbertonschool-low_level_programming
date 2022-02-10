524f802f758ce97fdbf97f62903c7a113b4b3fda
dianabanguera <dianabangueravillalobo@gmail.com>
2022-02-10 11:03:59 -0800

    puts imprime

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /* Puts = Imprime información */
 /* Return, debe devolver valor 0 */
+/* Main = Representa el inicio de ejecución */
+/* void = no retorna ningún valor */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
