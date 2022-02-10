29eea4db9a367392e8f3c977976b8c9cc64cf9ce
dianabanguera <dianabangueravillalobo@gmail.com>
2022-02-10 13:20:02 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
- *main - init
- *return : 0
-*/
+ * main - init
+ * Return: Always 0
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
