817a931e3219f7f9d3b31db725e2656e7e23ab38
dianabanguera <dianabangueravillalobo@gmail.com>
2022-02-10 12:55:38 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/* main - init */
-/* return :0 */
+/**
+* main - main block
+* return :0
+*/  
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
