373f9e4f3809120e05dba7a37a5e9c2392879083
dianabanguera <dianabangueravillalobo@gmail.com>
2022-02-11 15:46:08 -0800

    Prints number in base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <math.h>
 
+/**
+ * main - init
+ * Return: Always 0
+ */
 int main(void)
 {
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+	for (char l = 'a'; l <= 'f'; l++)
+		putchar(l);
+	putchar ('\n');
 	return (0);
 }
