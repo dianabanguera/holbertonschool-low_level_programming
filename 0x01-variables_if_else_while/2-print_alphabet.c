8f3f39afb9c5fdea3202c5e54b5e7c8ee00442c2
dianabanguera <dianabangueravillalobo@gmail.com>
2022-02-11 12:29:04 -0800

    alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - init
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putchar(alp);
+		putchar ('\n');
+	return (0);
+}
