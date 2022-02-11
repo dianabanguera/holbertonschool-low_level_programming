58b25d8cae47be769b45bd4fb103da0aadce1730
dianabanguera <dianabangueravillalobo@gmail.com>
2022-02-11 13:36:20 -0800

    alphabet except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - init
+ * Return: ALways 0
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph != 'q' && alph != 'e')
+			putchar(alph);
+	}
+	putchar ('\n');
+	return (0);
+}
