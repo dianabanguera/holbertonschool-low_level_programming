088a18b951df7451176d92e17c00509567807e8f
dianabanguera <dianabangueravillalobo@gmail.com>
2022-02-11 15:13:34 -0800

    numbers in ascii code

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <math.h>
 
+/**
+ * main - init
+ * Return: Always 0
+ */
 int main(void)
 {
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+	putchar ('\n');
 	return (0);
 }
