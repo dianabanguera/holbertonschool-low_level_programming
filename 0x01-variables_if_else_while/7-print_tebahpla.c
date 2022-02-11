7e6d038eee10c86b59c71bc2b48b018082d79ceb
dianabanguera <dianabangueravillalobo@gmail.com>
2022-02-11 15:27:07 -0800

    alphabet in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,17 @@
+#include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - init
+ * Return: Always 0
+ */
 int main(void)
 {
+	char alph;
+
+	for (alph = 'z'; alph >= 'a'; alph--)
+		putchar(alph);
+	putchar ('\n');
+
 	return (0);
 }
