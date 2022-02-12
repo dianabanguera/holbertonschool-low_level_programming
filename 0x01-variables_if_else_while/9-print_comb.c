4133f78e582496464b714777d0d753d57a7b8397
dianabanguera <dianabangueravillalobo@gmail.com>
2022-02-11 16:17:05 -0800

    combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
+/**
+ * main - init
+ * Return: Always 0
+ */
 int main(void)
 {
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(32);
+	}
+	putchar ('\n');
 	return (0);
 }
