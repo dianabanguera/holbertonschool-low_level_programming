60bab785a62357eac1fbd1076c7844ddd0198281
dianabanguera <dianabangueravillalobo@gmail.com>
2022-02-15 08:48:51 -0800

    verifica si el caracter está en minuscula

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - verifica que caracteres estan en minuscula
- *
+ * _islower - verifica que caracteres estan en minuscula
+ * @c: The character to be checked.
  * Return: Always 0
  */
 int _islower(int c)
@@ -10,8 +10,6 @@ int _islower(int c)
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
-	}else
-	{
-		return (0);
 	}
+	return (0);
 }
