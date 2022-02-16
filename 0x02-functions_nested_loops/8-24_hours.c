84e3f3fb5febeeb06055a238e57424c37b2c654e
dianabanguera <dianabangueravillalobo@gmail.com>
2022-02-15 18:04:27 -0800

    Imprime las horas del dia de jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - 24 horas
+ *
+ * Return: ALways 0
+ */
+void jack_bauer(void)
+{
+	int hora;
+	int min;
+
+	for (hora = 0; hora < 24; hora++)
+	for (min = 0; min < 60; min++)
+	{
+		_putchar((hora / 10) + '0');
+		_putchar((hora % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar('\n');
+	}
+}
