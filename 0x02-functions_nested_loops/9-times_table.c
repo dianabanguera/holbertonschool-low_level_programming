f66a066177c45be34ccf61325201e842de3ce210
dianabanguera <dianabangueravillalobo@gmail.com>
2022-02-15 18:18:28 -0800

    Imprime las tabla de multiplicar hasta el 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,12 +19,12 @@ void times_table(void)
 		_putchar(' ');
 
 		c = a * b;
-	
+
 	if (c <= 9)
 		_putchar(' ');
 	else
 		_putchar((c / 10) + '0');
-	_putchar((c % 10) +'0');
+	_putchar((c % 10) + '0');
 	}
 	_putchar('\n');
 	}
