495b485478e06dcfac853920575717b9d962e765
dianabanguera <dianabangueravillalobo@gmail.com>
2022-02-17 18:39:56 -0800

    Imprime los numeros del 0 al 14 10 veces

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,13 @@
 void more_numbers(void)
 {
 	int c;
-	int d = 0;
+	int d;
 
-	while (d++ <= 9)
+	for (d = 0; d <= 9; d++)
 	{
 		for (c = 0; c <= 14; c++)
 		{
+			if (c > 9)
 			_putchar((c / 10) + '0');
 			_putchar((c % 10) + '0');
 		}
