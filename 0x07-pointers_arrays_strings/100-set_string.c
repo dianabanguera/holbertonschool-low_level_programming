ec0746770adb94411931a806428371a75a3de153
dianabanguera <dianabangueravillalobo@gmail.com>
2022-03-02 20:23:06 -0800

    Establece el valor de un puntero a un caracter

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Establece el valor de un puntero a un caracter
+ *
+ * @s: puntero doble
+ * @to: char
+ * Return: 0 always.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
