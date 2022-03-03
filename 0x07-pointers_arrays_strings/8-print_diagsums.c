6e630b748e5f293b51c868d66b0c3e435a76055a
dianabanguera <dianabangueravillalobo@gmail.com>
2022-03-02 20:16:15 -0800

    Suma de dos diagonales de una matriz

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,22 +2,23 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - Imprime la sume de dos diagonales de una matriz de enteros
+ * print_diagsums - Imprime la sume de dos diagonales
  * @a: Matriz de enteros
  * @size: Tamaño de la matriz
  */
 void print_diagsums(int *a, int size)
 {
-	int i;
+int i;
 
-	unsigned int sum, sum1;
-	sum = 0;
-	sum1 = 0;
+unsigned int sum, sum1;
 
-	for (i = 0; i < size; i++)
-	{
-		sum += a[(size * i) + i];
-		sum1 += a[(size * (i + 1)) - (i + 1)];
-	}
-	printf("%d, %d\n", sum, sum1);
+sum = 0;
+sum1 = 0;
+
+for (i = 0; i < size; i++)
+{
+sum += a[(size * i) + i];
+sum1 += a[(size * (i + 1)) - (i + 1)];
+}
+printf("%d, %d\n", sum, sum1);
 }
