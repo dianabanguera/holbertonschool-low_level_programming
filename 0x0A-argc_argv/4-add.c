c2d5c92befa4269012d6c2690d21279316907729
dianabanguera <dianabangueravillalobo@gmail.com>
2022-03-04 20:33:54 -0800

    Suma de dos positivos

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Imprime la suma de dos numeros positivos
+ * @argc: Numero de argumentos
+ * @argv: Array de argumentos
+ * Return: 1 si uno de los digitos no es numero, de lo contrario 0
+ */
+int main(int argc, char *argv[])
+{
+int num, digit, sum = 0;
+
+for (num = 1; num < argc; num++)
+{
+for (digit = 0; argv[num][digit]; digit++)
+{
+if (argv[num][digit] < '0' || argv[num][digit] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[num]);
+}
+printf("%d\n", sum);
+return (0);
+}
