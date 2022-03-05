f5fc6b49132e0f9fc2f40ba688e9894068afc9c8
dianabanguera <dianabangueravillalobo@gmail.com>
2022-03-04 20:07:33 -0800

    Imprime todos los argumentos que recive

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Imprime todos los argumentos que recive
+ * @argc: Numero de argumentos
+ * @argv: Array de argumentos
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int arg;
+
+	for (arg = 0; arg < argc; arg++)
+			printf("%s\n", argv[arg]);
+
+	return (0);
+}
