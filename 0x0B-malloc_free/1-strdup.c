1b5f1a24808762d5a40b062a83e198ce9d470860
dianabanguera <dianabangueravillalobo@gmail.com>
2022-03-08 19:38:31 -0800

    Retorna un puntero a un nuevo espacio de memoria

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - function that returns a pointer to a newly allocated space in memory
+ * @str: string a copiar
+ * Return: Null si str es null o no hay suficiente espacio
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int i;
+	int l = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		l++;
+
+	dup = malloc(sizeof(char) * (l + 1));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		dup[i] = str[i];
+
+	dup[l] = '\0';
+
+	return (dup);
+}
