327de5064ac86970741a8ceb87875a863df6ee5e
dianabanguera <dianabangueravillalobo@gmail.com>
2022-03-08 19:02:26 -0800

    Imprime un array de caracteres

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - crea un array de caracteres y se inicialice con un caracter en especifíco
+ * @size: Tamaño de array
+ * @c: caracter con el que va iniciar el caracter
+ *
+ * Return: Si el tanmaño es 0 o falla es NULL de lo contrario un puntero al array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	return (array);
+}
