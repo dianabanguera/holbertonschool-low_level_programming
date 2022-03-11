86979fd20e2bd84d375214857e41fd081d846736
dianabanguera <dianabangueravillalobo@gmail.com>
2022-03-11 11:44:05 -0800

    Concatena dos string

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - function that concatenates two strings.
+ * @s1: string uno
+ * @s2: string dos
+ * @n: numero de bytes del s2 a concatenar
+ * Return: puntero
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	char *b;
+
+	if (!s1)
+	{
+		s1 = "";
+	}
+	if (!s2)
+	{
+		s2 = "";
+	}
+	b = malloc(sizeof(char) * strlen(s1) + n + 1);
+	if (!b)
+	{
+		return (NULL);
+	}
+	for (; i < (strlen(s1) + n); i++)
+	{
+		if (i < strlen(s1))
+		{
+			b[i] = s1[i];
+		}
+		else
+		{
+			b[i] = s2[j++];
+		}
+	}
+	b[i] = '\0';
+	return (b);
+}
