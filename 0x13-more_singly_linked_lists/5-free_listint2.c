8d715075222e9152def732c4f6d4b0b02d252ff5
dianabanguera <dianabangueravillalobo@gmail.com>
2022-04-01 09:48:34 -0700

    Frees a list2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a listint_t
+ * @head: head
+ * Return: 0
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next_node;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		next_node = (*head)->next;
+		free(*head);
+		*head = next_node;
+	}
+	*head = NULL;
+}
