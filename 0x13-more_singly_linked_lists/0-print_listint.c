c3bc01208f9b4200f59e2250a0068ee8867fe122
dianabanguera <dianabangueravillalobo@gmail.com>
2022-04-01 08:13:34 -0700

    all

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,9 +3,10 @@
 #include <stdio.h>
 
 /**
- * print_listint - function that prints all the elements of a list
- * @h: list
- * Return: counter
+ * print_listint - prints all the elements of a linked list
+ * @h: head node
+ *
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
