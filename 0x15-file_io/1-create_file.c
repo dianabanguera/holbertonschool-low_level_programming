885a87a26043ec56a2ceb46ef34fe148faa5db70
dianabanguera <dianabangueravillalobo@gmail.com>
2022-04-06 09:14:30 -0700

    Creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: pointer to name
+ * @text_content: pointer to string
+ * Return: 1 on sucess, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fileopen, filewrite, i;
+
+	if (filename == NULL)
+		return (-1);
+	fileopen = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fileopen == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(fileopen);
+		return (1);
+	}
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	filewrite = write(fileopen, text_content, i);
+	if (filewrite == -1)
+		return (-1);
+	close(fileopen);
+	return (1);
+}
