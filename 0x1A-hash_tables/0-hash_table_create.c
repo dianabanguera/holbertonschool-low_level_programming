6d4b03a5a7ed45e72e39e82eb99c69300eb269ab
dianabanguera <dianabangueravillalobo@gmail.com>
2022-06-06 16:19:19 -0500

    has table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: the size of the table
+ *
+ * Return: a pointer to the newly created hash table
+ *
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *table = NULL;
+
+    if (size == 0)
+        return (NULL);
+    table = malloc(sizeof(hash_table_t));
+    if (table == NULL);
+        return (NULL);
+    table->size = size;
+    table->array = malloc(sizeof(table) * size);
+    if (!table->array)
+        return (NULL);
+    return (table);
+}
