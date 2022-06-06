c2466bcb1fa9e42992d1423707cf61fcf46a2c28
dianabanguera <dianabangueravillalobo@gmail.com>
2022-06-06 15:17:16 -0700

    Create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,16 +9,16 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table = NULL;
+	hash_table_t *table = NULL;
 
-    if (size == 0)
-        return (NULL);
-    table = malloc(sizeof(hash_table_t));
-    if (table == NULL);
-        return (NULL);
-    table->size = size;
-    table->array = malloc(sizeof(table) * size);
-    if (!table->array)
-        return (NULL);
-    return (table);
+	if (size == 0)
+		return (NULL);
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = malloc(sizeof(table) * size);
+	if (!table->array)
+		return (NULL);
+	return (table);
 }
