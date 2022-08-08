2157f2d0317d08d293471785c06934e73fc5f3d0
dianabanguera <dianabangueravillalobo@gmail.com>
2022-08-08 14:55:18 -0500

    main.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,6 +2,7 @@
 #define SERACH_ALGOS_H
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
